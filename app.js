6207382aa2d79f64a9f62357cd16ace88f33b5db
shubh-37 <shubh.arya37@gmail.com>
2022-07-05 21:15:44 +0530

    installed packages and express server setup

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('04 Store API')
+require('dotenv').config();
+//async errors
+
+
+
+const express = require('express');
+const app = express();
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Store API</h1> <a href = "/api/v1/products"> products route</a>')
+})
+
+//product routes
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const start = async ()=>{
+    try{
+        //connectDB
+        app.listen(PORT, ()=>{
+            console.log(`Server running on port: ${PORT}...`)
+        });
+    }catch(error){
+        console.log(error);
+    }
+}
+
+start();
